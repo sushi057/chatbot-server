b090af7888f257ab92ed7d54d84757da83363ff7
Suvash Joshi <suvash2077@gmail.com>
2024-05-06 23:08:23 +0545

    feat: defautl deploy route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SalaryModule } from './salary/salary.module';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [SalaryModule, ConfigModule.forRoot()],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
