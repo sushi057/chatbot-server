69d11f7fb289920b3da67bf9c95cf5807802839c
Suvash Joshi <suvash2077@gmail.com>
2024-05-08 00:16:49 +0545

    fix: cors vercel

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+  });
   await app.listen(3001);
 }
 
